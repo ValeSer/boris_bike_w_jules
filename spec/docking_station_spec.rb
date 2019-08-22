c2d6c9093953a590f6bcf9aa0df86000ff4ffd0a
Valeria Serci <balerty84@gmail.com>
2019-08-22 16:39:08 +0100

    Add test for Bike, add a test less readable but shorter, update release_bike and 'working?' method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,18 @@
 require 'docking_station'
 
 describe DockingStation do
-  it " expects release_bike to be a method of docking_station" do
+  it "expects release_bike to be a method of docking_station" do
     is_expected.to respond_to(:release_bike)
   end
+
+  it "gets a bike" do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(bike.working?).to eq true
+  end
+
+  it "gets a bike 2 the less readable version" do
+    expect(DockingStation.new.release_bike.working?).to eq true
+  end
+
 end
