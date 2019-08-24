28079c4e77b2ae2fb81ee075dd1c0c2c08ab5598
Valeria Serci <balerty84@gmail.com>
2019-08-24 20:29:54 +0100

    Add private method full? and empty?

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,12 +7,23 @@ class DockingStation
   end
 
   def release_bike
-    raise 'Sorry, no bikes available' if @bikes.empty?
+    raise 'Sorry, no bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise 'Sorry, station is full' if @bikes.count >= 20
+    raise 'Sorry, station is full' if full?
     @bikes << bike
   end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
+  end
+
 end
